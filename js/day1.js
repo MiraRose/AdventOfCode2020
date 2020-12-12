b0feadacbff862afe1ef848123c55655ce84c885
MiraRose <mirandarregan@gmail.com>
2020-12-12 15:34:23 -0500

    Added ability to submit form and get answer to day 1 challenge

--- js/day1.js
+++ js/day1.js
@@ -1,12 +1,13 @@
 function findNumsThatSumTo2020(listOfNums) {
 
-  numsThatSumTo2020 = null;
+  console.log(listOfNums)
+  var numsThatSumTo2020 = null;
 
   for (i = 0; i < listOfNums.length; i++) {
-    comparingNum = listOfNums[i]
+    var comparingNum = listOfNums[i]
     for (i2 = 0; i2 < listOfNums.length; i2++) {
-      comparedToNum = listOfNums[i2]
-      sameIndex = i == i2
+      var comparedToNum = listOfNums[i2]
+      var sameIndex = i == i2
       if (!sameIndex && comparingNum + comparedToNum == 2020) {
         numsThatSumTo2020 = [comparingNum, comparedToNum]
       }
@@ -18,10 +19,24 @@ function findNumsThatSumTo2020(listOfNums) {
   }
 
   function findNumsThatSumTo2020AndMultiply(listOfNums) {
-    numsThatSumTo2020 = findNumsThatSumTo2020(listOfNums)
+    var numsThatSumTo2020 = findNumsThatSumTo2020(listOfNums)
+    console.log(numsThatSumTo2020)
     return numsThatSumTo2020[0] * numsThatSumTo2020[1]
   }
 
+  function formSubmit() {
+    var listOfNums = document.getElementById('listOfNums2020').value;
+    listOfNumsArray = listOfNums.split('\n').map(function(item) {
+      return parseInt(item, 10);
+  });
+    console.log("Got here")
+    console.log(listOfNums)
+    console.log(listOfNumsArray)
+    var multiple = findNumsThatSumTo2020AndMultiply(listOfNumsArray);
+    console.log(multiple)
+    document.getElementById('result').innerText = multiple;
+}
 
   module.exports = { findNumsThatSumTo2020: findNumsThatSumTo2020, 
-    findNumsThatSumTo2020AndMultiply: findNumsThatSumTo2020AndMultiply }
+    findNumsThatSumTo2020AndMultiply: findNumsThatSumTo2020AndMultiply,
+  formSubmit: formSubmit }
