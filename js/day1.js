b70bc6fbe60c0e22a172118ac02bf104c53c1868
MiraRose <mirandarregan@gmail.com>
2020-12-12 13:29:04 -0500

    Tweaks to test, test passing

--- js/day1.js
+++ js/day1.js
@@ -1,4 +1,16 @@
-function findNumsThatSumTo2020(a) {
-    return null;
+function findNumsThatSumTo2020(listOfNums) {
+
+  numsThatSum2020 = null;
+
+  for (i = 0; i < listOfNums.length; i++) {
+    for (i2 = 0; i2 < listOfNums.length; i2++) {
+      if (listOfNums[i] + listOfNums[i2] == 2020) {
+        numsThatSum2020 = [listOfNums[i], listOfNums[i2]]
+      }
+    }
+
+  }
+
+    return numsThatSum2020;
   }
   module.exports = findNumsThatSumTo2020;
--- js/day1.test.js
+++ js/day1.test.js
@@ -1,6 +1,7 @@
 const findNumsThatSumTo2020 = require('./day1');
 
 test('given list finds two nums that sum to 2020', () => {
-  expect(findNumsThatSumTo2020([1721, 979, 366, 299, 675, 1456]))
-  .toBe([1721, 299]);
+    result = findNumsThatSumTo2020([1721, 979, 366, 299, 675, 1456]);
+    expect(result).toContain(299);
+    expect(result).toContain(1721);
 });
