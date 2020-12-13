a6fcc49a71ca7e25ae72dff328f44835c6fab88f
MiraRose <mirandarregan@gmail.com>
2020-12-13 13:43:36 -0500

    Added test 'given list finds three  nums that sum to 2020'  and some clean up

--- js/day1.js
+++ js/day1.js
@@ -1,16 +1,19 @@
-function findNumsThatSumTo2020(listOfNums) {
+function findNumsThatSumTo2020(listOfNums, numOfNumsToSum) {
 
-  console.log(listOfNums)
   var numsThatSumTo2020 = null;
 
   for (i = 0; i < listOfNums.length; i++) {
     var comparingNum = listOfNums[i]
     for (i2 = 0; i2 < listOfNums.length; i2++) {
       var comparedToNum = listOfNums[i2]
-      var sameIndex = i == i2
-      if (!sameIndex && comparingNum + comparedToNum == 2020) {
-        numsThatSumTo2020 = [comparingNum, comparedToNum]
+
+      if (numOfNumsToSum == 2) {
+        var sameIndex = i == i2
+        if (!sameIndex && comparingNum + comparedToNum == 2020) {
+          numsThatSumTo2020 = [comparingNum, comparedToNum]
+        }
       }
+      
     }
 
   }
@@ -18,8 +21,8 @@ function findNumsThatSumTo2020(listOfNums) {
     return numsThatSumTo2020;
   }
 
-  function findNumsThatSumTo2020AndMultiply(listOfNums) {
-    var numsThatSumTo2020 = findNumsThatSumTo2020(listOfNums)
+  function findNumsThatSumTo2020AndMultiply(listOfNums, numOfNumsToSum) {
+    var numsThatSumTo2020 = findNumsThatSumTo2020(listOfNums, numOfNumsToSum)
     console.log(numsThatSumTo2020)
     return numsThatSumTo2020[0] * numsThatSumTo2020[1]
   }
@@ -29,11 +32,7 @@ function findNumsThatSumTo2020(listOfNums) {
     listOfNumsArray = listOfNums.split('\n').map(function(item) {
       return parseInt(item, 10);
   });
-    console.log("Got here")
-    console.log(listOfNums)
-    console.log(listOfNumsArray)
     var multiple = findNumsThatSumTo2020AndMultiply(listOfNumsArray);
-    console.log(multiple)
     document.getElementById('result').innerText = multiple;
 }
 
